32d38faa1bec8bc60487e4fe5247afa7a5e6295d
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-29 13:42:02 +0700

    #2 - Create upload & upload proses route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/upload', 'UploadController@upload');
+Route::post('/upload/proses', 'UploadController@proses_upload');
